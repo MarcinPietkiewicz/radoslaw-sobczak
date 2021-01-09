6a7f74d4fd47865e7878dd6259b5c41096ac60dd
Marcin <marcin.p.dev@gmail.com>
2021-01-09 01:24:44 +0100

    Changed language in login, added button to go back to the main page

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
 
 if (isset($logins[$Username]) && $logins[$Username] == $Password){
 $_SESSION['Username']=$logins[$Username];
-header("location:protected.php");
+header("location:edit.php");
 exit;
 } else {
 $msg="<span style='color:red'>Nieprawidłowe hasło</span>";
@@ -23,19 +23,23 @@ $msg="<span style='color:red'>Nieprawidłowe hasło</span>";
     </tr>
     <?php } ?>
     <tr>
-      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+      <td colspan="2" align="center" valign="top"><h3>Login</h3></td>
     </tr>
     <tr>
-      <td align="right" valign="top">Username</td>
+      <td align="right" valign="top">Nazwa użytkownika</td>
       <td><input name="Username" type="text" class="Input"></td>
     </tr>
     <tr>
-      <td align="right">Password</td>
+      <td align="right">Hasło</td>
       <td><input name="Password" type="password" class="Input"></td>
     </tr>
     <tr>
       <td> </td>
-      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+      <td><input name="Submit" type="submit" value="Login"></td>
+      </form>
+      </tr>
+      <tr>
+      <td> </td>
+      <td><form action="index.php" method="post"><input type="submit" value="Powrót do strony głównej"></form></td>
     </tr>
   </table>
-</form>
